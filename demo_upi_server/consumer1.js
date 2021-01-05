9380cd4a4ebe8611f3b75f4dcb6fa99ff2e6c59a
MankalaNithish <nithish.fourns@gmail.com>
2021-01-05 13:05:27 +0530

    Added Second consumer for saving transaction into database

---
+++ demo_upi_server/consumer1.js
@@ -0,0 +1,44 @@
+const { Kafka } = require('kafkajs')
+const config = require('./config')
+const mongoose = require('mongoose')
+const TransUser=require('./models/Transaction')
+
+const kafka = new Kafka({
+  clientId: config.kafka.CLIENTID,
+  brokers: config.kafka.BROKERS
+})
+
+mongoose.connect('mongodb://localhost:27017/upi',{
+    useNewUrlParser:true,
+},(err,result)=>{
+    if(err)
+        throw err
+    else{
+        console.log("Db connected")
+    }
+})
+
+const topic = config.kafka.TOPIC
+const consumer = kafka.consumer({
+  groupId: config.kafka.GROUPID
+})
+const run = async () => {
+    await consumer.connect()
+    await consumer.subscribe({ topic, fromBeginning: true })
+    await consumer.run({
+      eachMessage: async ({ message }) => {
+        try{
+            const jsonObj = JSON.parse(message.value.toString())
+            // sendMailToReceiver(jsonObj.receiver.email,jsonObj.amount,jsonObj.date)
+            // sendMailToSender(jsonObj.sender.email,jsonObj.amount,jsonObj.date)
+            console.log(jsonObj)
+            let result = await TransUser({receiver:jsonObj.receiver,sender:jsonObj.sender,amount:jsonObj.amount,date:jsonObj.date})
+            result.save()
+        }
+        catch(err){
+            console.log(err)
+        }
+      }
+    })
+  }
+  run().catch(e => console.error(`[example/consumer] ${e.message}`, e))
